5fc6b8142504402d21b186d86f039d55e8016237
ashaffir <alfred.shaffir@gmail.com>
2025-06-13 17:29:31 +0300

    fix certificate

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
-
-trap exit TERM
-
-while :; do
-    sleep 24h & wait $!
-    certbot renew --webroot -w /var/www/certbot --post-hook "nginx -s reload"
-done
+#!/bin/bash
+docker run --rm \
+  -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
+  -v "$(pwd)/certbot/www:/var/www/certbot" \
+  certbot/certbot:v2.10.0 renew \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  --no-random-sleep-on-renew \
+  --verbose
 
